8570210915c9c06bc4735a6105b8e060f4204144
Yung Beef 4.2 <yungbeefbigbags@gmail.com>
2024-11-04 11:49:05 -0300

    System and runtime

--- src/balances.rs
+++ src/balances.rs
@@ -1,38 +1,43 @@
 use std::collections::BTreeMap;
 
+#[derive(Debug)]
 pub struct Pallet {
-	balances: BTreeMap<String, u128>,
+    balances: BTreeMap<String, u128>,
 }
 
 impl Pallet {
-	pub fn new() -> Self {
-		Self {
-			balances: BTreeMap::new()
-		}
-	}
+    pub fn new() -> Self {
+        Self {
+            balances: BTreeMap::new(),
+        }
+    }
 
-	pub fn set_balance(&mut self, who: &String, amount: u128) {
-		self.balances.insert(who.clone(), amount);
-	}
+    pub fn set_balance(&mut self, who: &String, amount: u128) {
+        self.balances.insert(who.clone(), amount);
+    }
 
-	pub fn balance(&self, who: &String) -> u128 {
-		*self.balances.get(who).unwrap_or(&0)
-	}
+    pub fn balance(&self, who: &String) -> u128 {
+        *self.balances.get(who).unwrap_or(&0)
+    }
 
-	pub fn transfer(&mut self, caller: &String, to: &String, amount: u128) -> Result<(), &'static str> {
-		let caller_old = self.balance(caller);
-		let to_old = self.balance(to);
+    pub fn transfer(
+        &mut self,
+        caller: String,
+        to: String,
+        amount: u128,
+    ) -> Result<(), &'static str> {
+        let caller_old = self.balance(&caller);
+        let to_old = self.balance(&to);
 
-		let caller_new = caller_old.checked_sub(amount).ok_or("Insufficient funds")?;
-		let to_new = to_old.checked_add(amount).ok_or("Overflow")?;
+        let caller_new = caller_old.checked_sub(amount).ok_or("Insufficient funds")?;
+        let to_new = to_old.checked_add(amount).ok_or("Overflow")?;
 
-		self.set_balance(caller, caller_new);
-		self.set_balance(to, to_new);
+        self.balances.insert(caller, caller_new);
+        self.balances.insert(to, to_new);
 
-		Ok(())
-	}
+        Ok(())
+    }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -46,16 +51,19 @@ mod tests {
         assert_eq!(balances.balance(&"bob".to_string()), 0);
     }
 
-	fn test_transfer() {
-		let mut balances = super::Pallet::new();
+    fn test_transfer() {
+        let mut balances = super::Pallet::new();
 
-		balances.set_balance(&"alice".to_string(), 100);
-		assert_eq!(balances.balance(&"alice".to_string()), 100);
+        balances.set_balance(&"alice".to_string(), 100);
+        assert_eq!(balances.balance(&"alice".to_string()), 100);
 
-		balances.transfer(&"alice".to_string(), &"bob".to_string(), 50);
-		assert_eq!(balances.balance(&"alice".to_string()), 50);
-		assert_eq!(balances.balance(&"bob".to_string()), 50);
+        balances.transfer(&"alice".to_string(), &"bob".to_string(), 50);
+        assert_eq!(balances.balance(&"alice".to_string()), 50);
+        assert_eq!(balances.balance(&"bob".to_string()), 50);
 
-		assert_eq!(balances.transfer(&"alice".to_string(), &"bob".to_string(), 100), Err("Insufficient funds"));
-	}
+        assert_eq!(
+            balances.transfer(&"alice".to_string(), &"bob".to_string(), 100),
+            Err("Insufficient funds")
+        );
+    }
 }
